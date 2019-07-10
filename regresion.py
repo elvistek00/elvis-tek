27d00ce95b01ff2ed0a17582c5f869595284a2af
alejandro martinez <thompsonkiller@gmail.com>
2019-07-10 18:40:12 +0000

    Ejercicios de Python

---
+++ regresion.py
@@ -0,0 +1,9 @@
+#import
+import matplotlib.pyplot as plt
+import numpy as np
+
+#importing from scikit-learn
+#from sklearn import datasets, linear_model
+
+#load dataset
+#house_price = [245,321,279,308,199,219,
